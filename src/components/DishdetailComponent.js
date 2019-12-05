256b3b187747b0863cf3a9fc06ab65556be9aaac
Ya-Ting Lin <da18787@bristol.ac.uk>
2019-12-05 17:53:24 +0000

    Redux Thunk

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -3,6 +3,7 @@ import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb ,BreadcrumbIte
     Button, Row, Col, Label, Modal, ModalHeader, ModalBody } from 'reactstrap';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
+import { Loading } from './LoadingComponent';
 
     // function RenderDish(props){}
     function RenderDish({dish}) {
@@ -62,7 +63,25 @@ import { Link } from 'react-router-dom';
     }
     
     const DishDetail = (props) => {
-        if (props.dish != null) {
+        if (props.isLoading) {
+            return(
+                <div className="container">
+                    <div className="row">            
+                        <Loading />
+                    </div>
+                </div>
+            );
+        }
+        else if (props.errMess) {
+            return(
+                <div className="container">
+                    <div className="row">            
+                        <h4>{props.errMess}</h4>
+                    </div>
+                </div>
+            );
+        }
+        else if (props.dish != null) {
             return (
                 <div className="container">
                     <div className="row">
--- src/redux/ActionTypes.js
+++ src/redux/ActionTypes.js
@@ -1,2 +1,5 @@
+// action type
 export const ADD_COMMENT = 'ADD_COMMENT';
-
+export const DISHES_LOADING = 'DISHES_LOADING';  //DISHES_LOADING means the dishes are currently being fetched.
+export const DISHES_FAILED = 'DISHES_FAILED'; //DISHES_FAILED means that you failed to fetch the dishes information from a server
+export const ADD_DISHES = 'ADD_DISHES'; // ADD_DISHES means you want to add the dishes into your store
